96d5a754a5d0017e58532cb01415829bd7935539
VANI SATHVIKA REDDY <153749239+vanisathvika@users.noreply.github.com>
2024-06-22 16:42:23 +0530

    server.js

--- week-7/server.js
+++ week-7/server.js
@@ -1,4 +1,3 @@
-// Import express for creating API's endpoints
 const express = require("express");
 const path = require('path');
 const fs = require("fs");
@@ -6,17 +5,12 @@ const users = require("./database.json");
 var database;
 var token;
 
-// Import jwt for API's endpoints authentication
 const jwt = require("jsonwebtoken");
 
-// Creates an Express application, initiate
-// express top level function
 const app = express();
 
-// A port for serving API's
 const port = 3000;
 
-// Allow json data
 app.use(express.json());
 
 app.get('/',
@@ -36,53 +30,26 @@ app.get('/login',
         res.sendFile(__dirname + '/login.html');
     });
 
-// SignUp route
 app.post("/register", (req, res) => {
-	// Get the name to the json body data
 	const name = req.body.name;
-
-	// Get the password to the json body data
 	const password = req.body.password;
-
-	// Get the profession to the json body data
 	const work = req.body.work;
 	
-	// Make two variable for further use
 	let isPresent = false;
 	let isPresentIndex = null;
 
-    // Read database.json file 
     fs.readFile("database.json", function(err, data) { 
-    
-        // Check for errors 
         if (err) throw err; 
-
-        // Converting to JSON 
         database = JSON.parse(data); 
 
-	// iterate a loop to the data items and
-	// check what data are matched.
 	for (let i = 0; i < database.length; i++) {
-
-		// If data name are matched so check
-		// the password are correct or not
-		if (database[i].name === name
-			&& database[i].password === password) {
-
-			// If both are correct so make 
-			// isPresent variable true
+		if (database[i].name === name && database[i].password === password) {
 			isPresent = true;
-
-			// And store the data index
 			isPresentIndex = i;
-
-			// Break the loop after matching successfully
 			break;
 		}
 	}
 
-	// If isPresent is true, then create a
-	// token and pass to the response
 	if (isPresent) {
 		res.json({
 			signup: false,
@@ -102,10 +69,7 @@ app.post("/register", (req, res) => {
 			"database.json",
 			JSON.stringify(users),
 			err => {
-				// Checking for errors 
 				if (err) throw err;
-		
-				// Success 
 				res.json({
 					signup: true,
 					token: "generated",
@@ -117,68 +81,35 @@ app.post("/register", (req, res) => {
 });
 }); 
 
-
-// Login route
 app.post("/auth", (req, res) => {
-	// Get the name to the json body data
-	
 	const name = req.body.name;
 	console.log(name);
 
-	// Get the password to the json body data
 	const password = req.body.password;
 	console.log(password)
-	// Make two variable for further use
 	let isPresent = false;
 	let isPresentIndex = null;
 
-    // Read database.json file 
     fs.readFile("database.json", function(err, data) { 
-    
-        // Check for errors 
         if (err) throw err; 
-
-        // Converting to JSON 
         database = JSON.parse(data); 
 
-	// iterate a loop to the data items and
-	// check what data are matched.
 	for (let i = 0; i < database.length; i++) {
-
-		// If data name are matched so check
-		// the password are correct or not
-		if (database[i].name === name
-			&& database[i].password === password) {
-
-			// If both are correct so make 
-			// isPresent variable true
+		if (database[i].name === name && database[i].password === password) {
 			isPresent = true;
-
-			// And store the data index
 			isPresentIndex = i;
-
-			// Break the loop after matching successfully
 			break;
 		}
 	}
 
-	// If isPresent is true, then create a
-	// token and pass to the response
 	if (isPresent) {
-
-		// The jwt.sign method are used
-		// to create token
 		const token = jwt.sign(database[isPresentIndex], "secret");
-
-		// Pass the data or token in response
 		res.json({
 			login: true,
 			token: token,
 			data: database[isPresentIndex],
 		});
 	} else {
-
-		// If isPresent is false return the error
 		res.json({
 			login: false,
 			error: "please check name and password.",
@@ -187,26 +118,16 @@ app.post("/auth", (req, res) => {
 });
 }); 
 
-// Verify route
 app.post("/verifyToken", (req, res) => {
-
-	// Get token value to the json body
 	const token = req.body.token;
 
-	// If the token is present
 	if (token) {
-
-		// Verify the token using jwt.verify method
 		const decode = jwt.verify(token, "secret");
-
-		// Return response with decode data
 		res.json({
 			login: true,
 			data: decode,
 		});
 	} else {
-
-		// Return response with error
 		res.json({
 			login: false,
 			data: "error",
@@ -224,8 +145,6 @@ app.post('/signup',(req, res) => {
 	res.redirect("/signup")
 	});
 
-// Listen the server
 app.listen(port, () => {
-	console.log(`Server is running : 
-	http://localhost:${port}/`);
+	console.log(`Server is running : http://localhost:${port}/`);
 });
